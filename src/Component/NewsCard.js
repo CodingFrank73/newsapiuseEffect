a1c096ea26ec25b9dfd3784390a74080cdf3ffd6
Frank <codingfrank73@gmail.com>
2022-03-09 20:27:19 +0100

    cleanup deaktiviert

--- src/Component/NewsCard.js
+++ src/Component/NewsCard.js
@@ -1,44 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import Collapsible from 'react-collapsible'
 import './NewsCard.css'
 
 const NewsCard = () => {
 
     const [newsType, setNewsType] = useState('apple')
-    // const [lang, setNewLang] = useState(['de', 'en', 'fr'])
+    const [lang, setNewLang] = useState('de')
     const [newsArr, setNewsArr] = useState([])
 
 
-    // const Add = lang.map(Add => Add)
-
-    // const handleAddrTypeChange = (e) => {
-    //     console.log((lang[e.target.value]));
-    //     setNewLang(lang[e.target.value])
-
-    // }
+    const handleChange = (newValue) => {
+        setNewLang(newValue)
+    }
 
 
     useEffect(() => {
-        let loaded = true
+        // let loaded = true
         console.log("Inhalt wurde neu gerendert");
 
-        fetch(`https://newsapi.org/v2/everything?q=${newsType}&language=de&sortBy=popularity&apiKey=ceb65a2f9f8c4febad5a5af02d864573`)
+        fetch(`https://newsapi.org/v2/everything?q=${newsType}&language=${lang}&sortBy=popularity&apiKey=ceb65a2f9f8c4febad5a5af02d864573`)
             .then(response => response.json())
             .then(json => {
-                if (loaded) {
-                    console.log(json.articles);
-                    setNewsArr(json.articles)
-                }
-
+                // if (loaded) {
+                console.log(json.articles);
+                setNewsArr(json.articles)
             })
+        // })
 
-        // console.log(newsArr);
+        // return () => {
+        //     loaded = false;
+        //     console.log('process stopped');
+        // }
 
-        return () => {
-            loaded = false;
-            console.log('process stopped');
-        }
-
-    }, [newsType])
+    }, [newsType, lang])
 
     return (
         <>
@@ -47,16 +41,36 @@ const NewsCard = () => {
                     <button onClick={() => setNewsType('apple')}> Apple</button>
                     <button onClick={() => setNewsType('tesla')}>Tesla</button>
                     <button onClick={() => setNewsType('war')}>War</button>
+
+                    <select name="" id="" onChange={(e) => handleChange(e.target.value)} value={lang} >
+                        <option value='de' >Deutsch</option>
+                        <option value='en'>Englisch</option>
+                        <option value='fr'>Französisch</option>
+                    </select>
+
+                    <input type="text" name="" id="" />
                 </article>
 
                 <div className="flex">
                     {newsArr.map((items) => (
+
+
                         <article className="cards">
-                            <img src={items.urlToImage} alt="pic" />
+
+                            <img src={items.urlToImage} onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = "/schwarz.jpeg";
+                            }} alt="pic" />
+
+
                             <h1>{items.title}</h1>
-                            <p>{items.description}</p>
-                            <p>{items.publishedAt}</p>
-                            <a href={items.url}>Read more</a>
+
+                            <Collapsible trigger="Start">
+                                <p>{items.description.slice(0, 150)}</p>
+                                <p>{items.publishedAt}</p>
+                                <a href={items.url}>Read more</a>
+                            </Collapsible>
+
                         </article >
                     ))}
                 </div>
@@ -66,16 +80,3 @@ const NewsCard = () => {
 }
 
 export default NewsCard
-
-
-//<select name="" id="" value={lang} onChange={e => setNewLang(e.target.value)}>
-                        //{lang.map((addres) => {
-                            //<option key={addres} value={addres}>{addres}</option>
-                            //console.log(lang);
-                        //})} */}
-
-
-
-//{/* <option value="de">Deutsch</option>
-                        //<option value="en">Englisch</option>
-                        //<option value="fr">Französisch</option> */}
